73502280e1372a6d7213c88192b9526e6e7e52d4
bg-changer <yashdt50@gmail.com>
2023-12-30 18:34:25 +0530

    fix: grammer error

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -39,12 +39,12 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>My Projects</p>
+        <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
       <div className="w-full flex">
         <motion.p variants={fadeIn('', '', 0.1, 1)} className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-          These projects below showcases the implementations of what I have learned so far in ReactJS and WebRTC. Some of them are also real-world examples. Links to code repositories are also present.
+        These projects below showcase the implementations of what I have learned so far in ReactJS and WebRTC. Some of them are also real-world examples, and links to code repositories are provided.
         </motion.p>
       </div>
 
